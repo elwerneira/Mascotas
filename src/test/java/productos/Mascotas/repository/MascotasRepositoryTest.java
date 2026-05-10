58da1a6ec6a5606e42b530b4c0aa50870c66fca0
elwerneira <el.neira@duocuc.cl>
2026-05-10 17:06:12 -0400

    add new feature

--- src/test/java/productos/Mascotas/repository/MascotasRepositoryTest.java
+++ src/test/java/productos/Mascotas/repository/MascotasRepositoryTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import productos.mascotas.entity.MascotaEntity;
-
 class MascotasRepositoryTest {
 
     @Test
